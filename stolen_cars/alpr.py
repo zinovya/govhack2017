1263a8b7a74e5255c918511d0e8f43a92c56f4fa
Admin <winstonthepoh@gmail.com>
2017-07-29 19:50:56 +1200

    handle edge cases

--- stolen_cars/alpr.py
+++ stolen_cars/alpr.py
@@ -8,18 +8,20 @@ class AlprCaller():
 
     def get_license_plate_text(self):
         try:
-            text = str(sp.check_output(['alpr', '-c ', str(self.country_code), self.image_name]))
+            text = str(sp.check_output(['alpr', '-c ', str(self.country_code), self.image_name])).strip('b').split('-')
+            text=text[1:]
         except sp.CalledProcessError:
             text = math.pi
             return text
-
-        if len(text)>3:
-            text = text.strip('b').split('-')[1:]
+        if text:
             text = [i.strip().strip('\\n').split('\\t confidence: ') for i in text][0]  # Only get guess with highest probability
             text= tuple((text[0] ,float(text[1])))
+        else:
+            text = math.pi
+            return text
 
         return text
 
-# A = AlprCaller('eu','aryan1.')
-# txt = A.get_license_plate_text()
-# print(txt)
+A = AlprCaller('eu','das638.jpeg')
+txt = A.get_license_plate_text()
+print(txt)
